d1675eac88eb14ff2b6ab3582587bbd6a0c76c1c
ditto-chaar <md.aqeelahmed007@gmail.com>
2026-01-01 23:35:14 +0530

    feat: add cart slice logic

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState = {
+  cartItems : [],
+  amount: 0,
+  total:0
+} 
+
+const cartSlice = createSlice({
+  name:"cart",
+  initialState: initialState,
+  reducers :{
+
+  }
+})
+
+
+export default cartSlice.reducer
