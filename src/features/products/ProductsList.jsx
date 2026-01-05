fe75cd9a3c56f61c90c5acf69a0cea461d757d3c
ditto-chaar <md.aqeelahmed007@gmail.com>
2026-01-05 20:43:50 +0530

    style: polish product list with grid and cards

--- src/features/products/ProductsList.jsx
+++ src/features/products/ProductsList.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchProducts } from "./productsSlice"
 import { addItem } from "../cart/cartSlice"
+import "./ProductList.css"
 
 export function ProductsList(){
   const dispatch = useDispatch()
@@ -16,14 +17,18 @@ export function ProductsList(){
   }
 
   return (
-    <div className="products-grid">
-   {status === 'succeeded' &&  <ul>
+    <>
+   {status === 'succeeded' &&  <ul className="products-grid">
     {items.map((item)=>{
       return (
-        <li key={item.id}>{item.title} price: {item.price} <button onClick={()=>addBtn(item)}>add Item</button></li>
+        <div className="product-card" key={item.id}>
+          <img src={item.image} alt={item.title} />
+          <h4>{item.title}</h4>
+          <p>${item.price}</p>
+           <button onClick={()=>addBtn(item)}>add Item</button></div>
       )
     })}
     </ul>}
-    </div>
+    </>
   )
 }
