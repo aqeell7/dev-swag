01737ded42d6d78f2e3beb558f83f27f0f70851b
ditto-chaar <md.aqeelahmed007@gmail.com>
2026-01-09 14:17:45 +0530

    implement product api routes and controller

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import connectDb from './config/db.js'
+import Productroutes from './routes/productRoutes.js'
 
 const app = express()
 const PORT = 5000
 connectDb()
+
+app.use(express.json())
+app.use('/api/products',Productroutes)
 
 app.listen(PORT, ()=>{
   console.log(`server running on port ${PORT}`)
