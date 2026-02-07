a33c9796c411a4b5add4e1fc0e39350f0a3ee809
ditto-chaar <md.aqeelahmed007@gmail.com>
2026-02-07 23:43:02 +0530

    feat: setup auth dependencies and user model

---
+++ server/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  isAdmin: { 
+    type: Boolean, 
+    default: false 
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
